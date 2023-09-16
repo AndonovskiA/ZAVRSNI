0f3784faa73d32adedfe682a4571536f60b0d1a5
Ivor Ćelić <ivorcelic@gmail.com>
2023-09-16 11:31:20 +0200

    Changed

--- DSstart/DrivingSchoolWebApi/Controllers/CourseController.cs
+++ DSstart/DrivingSchoolWebApi/Controllers/CourseController.cs
@@ -3,8 +3,7 @@ using DrivingSchoolWebApi.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DrivingSchoolWebApi.Models;
-
-
+using System.Linq.Expressions;
 
 namespace DrivingSchoolWebApi.Controllers
 {
@@ -99,18 +98,18 @@ namespace DrivingSchoolWebApi.Controllers
             }
             try 
             {
-                var instructor = _context.Instructor.Find();
+                var instructor = _context.Instructor.Find(courseDTO.IDInstructor);
                 if (instructor == null) 
                 {
                     return BadRequest(ModelState);
                 }
-                var vehicle = _context.Vehicle.Find();
+                var vehicle = _context.Vehicle.Find(courseDTO.IDVehicle);
                 if(vehicle == null)
                 {
                     return BadRequest(ModelState);
                 }
 
-                var category = _context.Category.Find();
+                var category = _context.Category.Find(courseDTO.IDCategory);
                 if (category == null)
                 {
                     return BadRequest(ModelState);
@@ -130,11 +129,7 @@ namespace DrivingSchoolWebApi.Controllers
                 _context.SaveChanges();
 
                 courseDTO.ID = c.ID;
-                
-
-                
               
-
 
                 return Ok(courseDTO);
             }
@@ -143,8 +138,81 @@ namespace DrivingSchoolWebApi.Controllers
                 return StatusCode(StatusCodes.Status503ServiceUnavailable,ex);
             }
 
+        }
+
+        [HttpPut]
+        [Route("{ID:int}")]
+
+        public IActionResult Put(int ID, CourseDTO courseDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            if (ID <= 0 || courseDTO == null)
+            {
+                return BadRequest();
+            }
+
+            try
+
+            {
+                var course = _context.Course.Find(ID);
+                if (course == null)
+                {
+                    return BadRequest();
+                }
+                var instructor = _context.Instructor.Find(ID);
+                if (instructor == null)
+                {
+                    return BadRequest(ModelState);
+                }
+                var vehicle = _context.Vehicle.Find(ID);
+                if (vehicle == null)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var category = _context.Category.Find(ID);
+                if (category == null)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var student= _context.Student.Find(ID);
+                if (student == null)
+                {
+                    return BadRequest(ModelState);
+                }
+
+
+                // public int Number_of_students { get; set; }
+
+                     courseDTO.ID = ID;
+
+
+                return Ok(courseDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(
+                    StatusCodes.Status503ServiceUnavailable,
+                    ex.Message);
+            }
 
         }
+
+
+
+
+
+
+
+
+
+
+
+
     }   
 
 }
