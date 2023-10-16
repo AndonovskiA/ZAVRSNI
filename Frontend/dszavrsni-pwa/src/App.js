7a110dc1871d3df130f19b8256a18ce78c636903
Anja <andonovski.anja@gmail.com>
2023-10-16 18:45:45 +0200

    Frontend changes

--- Frontend/dszavrsni-pwa/src/App.js
+++ Frontend/dszavrsni-pwa/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainMenu from './Components/MainMenu.component'; 
+import MainPage from './Components/MainPage.component';
+import ControlBoard from './Components/ControlBoard.component';
+import addCategory from './Components/Category/addCategory.component';
+import Categories from './Components/Category/categories.component';
+import changeCategory from './Components/Category/changeCategory.component';
+import addInstructor from './Components/Instructor/addInstructor.component';
+import changeInstructor from './Components/Instructor/changeInstructor.component';
+import Instructors from './Components/Instructor/instructors.component';
+import addStudent from './Components/Student/addStudent.component';
+import changeStudent from './Components/Student/changeStudent.component';
+import Students from './Components/Student/students.component';
+import addVehicle from './Components/Vehicle/addVehicle.component';
+import changeVehicle from './Components/Vehicle/changeVehicle.component';
+import Vehicles from './Components/Vehicle/vehicles.component';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MainMenu />
+      <Routes>
+        <Route path= '/' element={<MainPage />} />
+        <Route path= '/ControlBoard' element={<ControlBoard />} />
+        <Route path= '/students' element={<Students />} />
+        <Route path= "/Student/add" element={<addStudent />} />
+        <Route path= "/Students/:ID" element={<changeStudent/>}/>
+        <Route path= "/Categories" element= {<Categories />} />
+        <Route path= "/Categories/add" element={<addCategory/>}/>
+        <Route path= "/Categories/change" element={<changeCategory/>}/>
+        <Route path= "/Instructors" element={<Instructors/>} />
+        <Route path= "/Instructors/add" element={<addInstructor/>}/>
+        <Route path= "/Instructors/change" element={<changeInstructor/>}/>
+        <Route path= "/Vehicles" element={<Vehicles/>}/>
+        <Route path= "/Vehicles/add" element={<addVehicle/>}/>
+        <Route path= "/Vehicles/change" element={<changeVehicle/>}/>
+
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
+
--- Frontend/dszavrsni-pwa/src/http-common.js
+++ Frontend/dszavrsni-pwa/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-    baseURL: "http://anjaandonovski-001-site1.btempurl.com/api/v1",
+    baseURL: "https://anjaandonovski-001-site1.btempurl.com/api/v1",
     headers: {
         "Content-Type": "application/json"
     }
